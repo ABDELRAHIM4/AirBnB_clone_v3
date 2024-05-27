7e964eda348da10463decd21b8c32d7f8e9407bf
ABDELRAHIM4 <abdulrhimsaeed@gmail.com>
2024-05-27 16:31:16 +0000

    AirBnb

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+"""Create an endpoint that retrieves the number of each objects by type"""
 from models import storage
 from models.user import User
 from models.state import State
@@ -10,9 +11,11 @@ from flask import jsonify
 from api.v1.views import app_views
 @app_views.route('/status', methods=['GET'])
 def view():
+	"""create a route /status on the object"""
 	return jsonify({"status": "OK"})
 @app_views.route('/stats', methods=['GET'])
 def stats():
+	"""Create an endpoint that retrieves the number """
 	stats = {}
 	stats["amenities"] = storage.count(Amenity)
 	stats["cities"] = storage.count(City)
