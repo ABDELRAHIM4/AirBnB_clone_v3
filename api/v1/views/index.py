db9a090cbca929f4b39f0aac150c0093b3fdadef
ABDELRAHIM4 <abdulrhimsaeed@gmail.com>
2024-05-27 14:17:15 +0000

    AirBnb

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,10 +1,4 @@
 #!/usr/bin/python3
-from api.v1.views import app_views
-from flask import jsonify
-@app_views.route('/status', methods=['GET'], strict_slashes=False)
-def status():
-    return jsonify ({"status": "OK"})
-=======
 from flask import jsonify
 from api.v1.views import app_views
 @app_views.route('/status', methods=['GET'])
